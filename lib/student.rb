38f43fc881110be7a16cb0396078372aaca1590e
Joann Pan <joannlpan@gmail.com>
2018-12-25 20:37:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,9 +3,10 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
-  def initialize (name, album)
-    @name
-    @album
+  def initialize (name, album, id=nil)
+    @name = name
+    @album = album
+    @id = id
   end
 
 
